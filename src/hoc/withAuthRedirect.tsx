98e9fb6be19a949473c1360870c0ecb25a3f7555
vasyokbelokopytov <guywithtwistedmind2001@gmail.com>
2021-09-14 01:46:09 +0300

    hocs typing

---
+++ src/hoc/withAuthRedirect.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { GlobalStateType } from '../redux/redux-store';
+import { selectIsAuth } from '../redux/selectors/auth-selectors';
+
+type MapStateToPropsType = {
+  isAuth: boolean;
+};
+
+function withAuthRedirect<P>(Component: React.ComponentType<P>) {
+  const containerComponent: React.FC<MapStateToPropsType> = ({
+    isAuth,
+    ...props
+  }) => {
+    if (!isAuth) return <Redirect to="login" />;
+    return <Component {...(props as P)} />;
+  };
+
+  const mapStateToProps = (state: GlobalStateType): MapStateToPropsType => ({
+    isAuth: selectIsAuth(state),
+  });
+
+  return connect<MapStateToPropsType, {}, P, GlobalStateType>(mapStateToProps)(
+    containerComponent
+  );
+}
+
+export default withAuthRedirect;
---
+++ src/hoc/withOwnerRedirect.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { Redirect, withRouter, RouteComponentProps } from 'react-router-dom';
+
+import { selectIsAuth } from '../redux/selectors/auth-selectors';
+import { GlobalStateType } from '../redux/redux-store';
+
+type MapStateToPropsType = {
+  isAuth: boolean;
+};
+
+type RouterParamsType = {
+  userId: string;
+};
+
+type RouterPropsType = RouteComponentProps<RouterParamsType>;
+
+function withOwnerRedirect<P>(Component: React.ComponentType<P>) {
+  debugger;
+  const containerComponent: React.FC<MapStateToPropsType & RouterPropsType> = (
+    props
+  ) => {
+    if (!props.isAuth && !props.match.params.userId)
+      return <Redirect to="login" />;
+    return <Component {...(props as unknown as P)} />;
+  };
+
+  const mapStateToProps = (state: GlobalStateType): MapStateToPropsType => ({
+    isAuth: selectIsAuth(state),
+  });
+
+  return compose<React.ComponentType<P>>(
+    connect<MapStateToPropsType, {}, P, GlobalStateType>(mapStateToProps),
+    withRouter
+  )(containerComponent);
+}
+
+export default withOwnerRedirect;
